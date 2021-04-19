bef2b7ea44ed7034e63487513eb628663030d245
Fenna <Fennahornstra@gmail.com>
2021-04-19 10:38:47 +0200

    Add project files.

---
+++ assignment2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace assignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //fibonacci to the tenth number
+            BasicFibonacci(int.MaxValue);
+
+
+            Console.ReadLine();
+        }
+
+        private static void BasicFibonacci(int sequenceLength)
+        {
+            long a = 0;
+            long b = 1;
+            Console.WriteLine($"{a}\n{b}");
+            for (int i = 0; i < sequenceLength; i++)
+            {
+                long c = a + b;
+                if (c < 0)
+                {
+                    Console.WriteLine(i+1);
+                    break;
+                }
+                a = b;
+                b = c;
+                Console.WriteLine(c);
+            }
+        }
+
+    }
+}
